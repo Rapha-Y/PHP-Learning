62e16a69625fb46a509b56ccbe4601b94b1d31fe
Rapha-Y <raph.yuki@gmail.com>
2020-09-03 16:21:03 -0300

    added include/require examples

---
+++ traversy/websiteOne/index.php
@@ -0,0 +1,9 @@
+<?php require('inc/header.php'); ?>
+<h1>Home</h1>
+<?php require 'inc/footer.php'; ?>
+
+<!--
+    include - script keeps running if file isn't found
+    require - script stops if file isn't found
+    require_once - checks if file has already been included/required
+-->
